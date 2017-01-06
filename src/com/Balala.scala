3c837e0219413ab98cfd5be1bcb8d039c5afa899
wupeng <857038419@qq.com>
2017-01-06 19:52:25 +0800

    hahah

--- src/com/Balala.scala
+++ src/com/Balala.scala
@@ -1,9 +1,5 @@
 package com
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
-import org.apache.log4j.{Level, Logger}
-
 /**
   * Created by wupeng1 on 2017/1/6.
   */
@@ -11,16 +7,26 @@ object Balala {
 
 
   def main(args: Array[String]): Unit = {
+    val r = eval("4+9+9")
+    println(r)
+  }
 
-
+  def eval(str: String): Int = str match {
+    case Add(expr1, expr2) => eval(expr1) + eval(expr2)
+    case _ => str.toInt
   }
 
 }
 
 object Add {
-  def apply(expr1: String, expr2: String) = expr1 + "+" + expr2
+  val op = "+"
+
+  //  def apply(expr1: String, expr2: String) = expr1 + "+" + expr2
   def unapply(str: String): Option[(String, String)] = {
-    val parts = str split ("\\+")
-    if (parts.length == 2) Some(parts(0), parts(1)) else None
+    val index = str indexOf op
+    if (index > 0)
+      Some(str substring(0, index), str substring (index + 1))
+    else
+      None
   }
 }
